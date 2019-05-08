df583cc20216c4be7620a0e9ec503ff5039b2e41
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2019-05-08 01:09:20 +0100

    designed d comment button, and fixed the comment button to take input and submit to database

--- blog.php
+++ blog.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+include 'db.php';
+
+include 'comment.php';
+date_default_timezone_set('Africa/Nigeria');
+
+
+
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,7 +45,7 @@
 <header>
         <?php       
             // echo "<p class='gmail'> yh </p> ";
-            $show = $_SESSION['f_name . l_name'];
+            $show = $_SESSION['f_name'];
             // echo "<p class='gmail'> Welcome  $show </p>";
             if (isset(/*$_SESSION['f_name']*/ $show )){
             echo "<p class='gmail'> Welcome! You are loggedin as $show </p>";
@@ -67,14 +79,37 @@
             </header>
 <div class="full_section">
            
+    <div class="images-container">       
             <iframe src="" frameborder="0" allowfullscreen></iframe>
         <br>
-        <form action="">
-            <input type="hideen" name="id" value='anonymmous'>
-            <input type="hideen" name="date" value='".date('Y-m-d H:i:s')."'> <br> <br>
-            <textarea name="message" id="" cols="30" rows="10"></textarea> <br>
-            <button type="submit" class="btn-signup" name="submit">Comment</button>
-        </form>
+<?php
+    echo
+        " <form method='POST' action='".setComments($conn)."'>
+            <input type='hidden' name='u_name' value='anonymmous'>
+            <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'> <br> <br>
+            <textarea name='message' id='' cols='30' rows='10'></textarea> <br>
+            <button type='submit' class='btn-signup' name='submitComment'>Comment</button>
+        </form>"
+    ?>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>            
 
 
@@ -92,114 +127,46 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <footer class="main-footer">
-            <div class="footer1">
-                <h5>Services</h5>
-                <p>Fashion Bundles</p>
-                <p>clothing neating</p>
-                <p>On delivery</p>           
-            </div>
-            <!-- <div style="clear:both"></div> -->
-            <div class="fosertercontent">
-            <div class="foosup">
-                <h5>Support</h5>
-                <p>Registration Tips</p>
-                <p>BallyApp</p>
-                <p>BallyGoogle</p>
-            </div>
-
-            <div class="foolife">
-                <h5>LifeStyle</h5>
-                <p>Islamic Attire</p>
-                <p>Islamic Modelling</p>
-                <p>Islamic Events</p>
-            </div>
+<footer class="main-footer">
+        <div class="footer1">
+            <h5>Services</h5>
+            <p>Fashion Bundles</p>
+            <p>clothing neating</p>
+            <p>On delivery</p>           
         </div>
-            <!-- <div style="clear:both"></div> -->
+        <!-- <div style="clear:both"></div> -->
+        <div class="foosup">
+            <h5>Support</h5>
+            <p>Registration Tips</p>
+            <p>BallyApp</p>
+            <p>BallyGoogle</p>
+        </div>
 
-            <div class="fooform">
-                <h4>NEWSLETTER Sign-Up</h4>
-                <p>SignUp to stay Updated</p>
-                <form action="" method="post">
-
-<!-- exclude this fr now 
-                    <div>
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" name="user_name" required placeholder="Enter your Full Name">
-                    </div>
--->
-<?php
-'<p class="gmail">User Mail Already Exist</p>';
-
-?>
-
-<div> 
-                        <label for="mail">E-mail:</label>
-                        <input type="email" id="mail" name="user_mail" required placeholder="Enter your Email address">
-                    </div>
-
-                    <div></div>
-
-                    <!-- JAVASCRIPT ADDED -->
-
-                    <button type="submit" name="submit" class="btn-signup">Click  to Sign-Up</button>
-                </form>            
-            </div>
-            <div class="sm">
-                <div class="fa fa-facebook fa-5x"></div>
-                <div class="fa fa-twitter fa-5x"></div>
-                <div class="fa fa-instagram fa-5x"></div>
-                <div class="fa fa-linkedin fa-5x"></div>
-
-            </div>
+        <div class="foolife">
+            <h5>LifeStyle</h5>
+            <p>Islamic Attire</p>
+            <p>Islamic Modelling</p>
+            <p>Islamic Events</p>
+        </div>
         
+        <!-- <div style="clear:both"></div> -->
 
-
-            <div style="clear:both"></div>
+        
+        <div style="clear:both"></div>
+        <div class="fooadd">
+            <address>                    
+                <i class="fa fa-phone fa-2x">Tel: <a href="tel: +234-081-8289-6889-">+234-081-8289-6889</a></i><br/>
+                <i class="fa fa-envelope-square fa-2x"> E-Mail: <a href="mailto:ajayidhikrullah@gmail.com">ajayidhikrullah@gmail.com</a></i><br/>
+                <hr>
+                <i class="fa fa-map-marker fa-5x"> <span class="p">2, Obokun Close, Allen Avenue, Ikeja Lagos state <br/></span></i>
+                <p class="p">&COPY; Copyright ajayidhikrullah2018</p>
+            </address>
             
-            <div class="fooadd">
-                <address>                    
-                    <i class="fa fa-phone fa-2x">Tel: <a href="tel: +234-081-8289-6889-">+234-081-8289-6889</a></i><br/>
-                    <i class="fa fa-envelope-square fa-2x"> E-Mail: <a href="mailto:ajayidhikrullah@gmail.com">ajayidhikrullah@gmail.com</a></i><br/>
-                    <hr>
-                    <i class="fa fa-map-marker fa-5x"> <span class="p">2, Obokun Close, Allen Avenue, Ikeja Lagos state <br/></span></i>
-                    <p class="p">&COPY; Copyright ajayidhikrullah2018</p>
-                </address>
-            </div>
-        </footer>
-        <script src="js/script.js"></script>
-  
+        </div>
+    </footer>
+    
+
+    <script src="js/script.js"></script>
+
 </body>
 </html>
