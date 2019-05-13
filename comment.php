eb90ce4aed4c7eb4138d9beb3bd58956189d0ce7
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2019-05-13 14:42:04 +0100

    fixed delete comment and edit comment of users

--- comment.php
+++ comment.php
@@ -1,7 +1,7 @@
 <?php
 // <!-- //we want to insert data to db --> if u want to insert to databas, use SETS
 function setComments($conn){
-	if(isset($_POST['submitComment']))/*is this is not set, dont do it*/ {	
+	if(isset($_POST['submitComment']))/*if this is not set, dont do it*/ {	
 		$u_name = $_POST['u_name'];
 		$date = $_POST['date'];
 		$msg = $_POST['message'];
@@ -22,23 +22,64 @@ function getComments($conn){
 	$result = $conn -> query($sql);
 	//fetch out the datas in d database
 	// $row = $result->fetch_assoc(); --> this will onlyt fetch one result frin d column
+	//echo out one data from the column
+	// echo $row['message'];
+
 	//lets loop through
 	while($row = $result->fetch_assoc()){
 		echo "<div class='show-comment'>";
 			echo $row['u_name'];
 			$date = $row['date'];
-			echo "<p class='ShowDate'> $date </p>";
+			echo "<p class='ShowDate'>$date</p>";
 			//lets show d line breaks to some long text
 			echo nl2br($row['message']) . "<br><br>"; 
 	
-		echo "</div>" . '<br>';
+		echo "
+		<!--Delete Below-->
+
+		<form method='POST' action='".deleteComments($conn)."' class='deleteBtn'>
+			<input type= 'hidden' name='id' value='".$row['id']."'>
+
+			<button name='commentDelete' type='submit'>Delete Comment</button>
+		</form>
+
+
+		<!-- Edit Below>-->
+		<form method='POST' action='editcomment.php' class='editBtn'>
+			<input type= 'hidden' name='id' value='".$row['id']."'>
+			<input type= 'hidden' name='u_name' value='".$row['u_name']."'>
+			<input type= 'hidden' name='date' value='".$row['date']."'>
+			<input type= 'hidden' name='message' value='".$row['message']."'>
+			<button>Edit</button>		
+			
+			</form>
+		</div>" . '<br>';
 
 	}
 
-	//echo out one data from the column
-	// echo $row['message'];
 
 }
 
+function editComments($conn){
+	if (isset($_POST['editComm'])){
+		$id = $_POST['id'];
+		$u_name = mysqli_real_escape_string($cnn, $_POST['u_name']);
+		$date = mysqli_real_escape_string($conn, $_POST['date']);
+		$msg = mysqli_real_escape_string($conn, $_POST['message']);
 
+		$sql = "UPDATE comments SET message='$msg' WHERE id='$id'";
+		$result = $conn -> query($sql);
+		header("Location: blog.php");
+	}
+}
+
+function deleteComments($conn){
+	if (isset($_POST['commentDelete'])){
+		$id = $_POST['id'];
+		
+		$sql = "DELETE FROM comments WHERE id='$id'";
+		$result = $conn -> query($sql);
+		header("Location: blog.php");
+	}
+};
 ?>
